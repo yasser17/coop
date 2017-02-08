53bdaca03fe2f414421a6bb5e2c4ea7b8ae4705f
Yasser <yasser@fx2.com.uy>
2017-02-08 17:59:25 -0300

    facebook

--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -1,3 +1,38 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+if (window.cordova.platformId == "browser") {
+    facebookConnectPlugin.browserInit("707821006054644");
+}
+
+function onDeviceReady()
+{
+
+}
+
+function fbLoginSuccess()
+{
+	$('#status').html('Login success');
+}
+
+function loginToFacebook()
+{
+	facebookConnectPlugin.login(["public_profile"],
+		fbLoginSuccess,
+		function (err) {
+			$('#status').html('Authentication fail');
+		}
+	);
+}
+
+function logout() 
+{
+	facebookConnectPlugin.logout(function () {
+		$('#status').html('Logut is successfull');
+	}, function (e) {
+		alert('error');
+	});
+}
+
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 })
