aaf14d6647a3cbdcba03b47a0b522111a4ca0a43
Yasser Mussa <yasser.mussa@gmail.com>
2017-02-09 08:47:59 -0300

    login

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -1,22 +1,53 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
-if (window.cordova.platformId == "browser") {
-    facebookConnectPlugin.browserInit("707821006054644");
-}
+
+window.fbAsyncInit = function() {
+	FB.init({
+	  appId      : '707821006054644',
+	  cookie     : true,
+	  xfbml      : true,
+	  version    : 'v2.8'
+	});
+};
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+new Vue({
+	el: '#app',
+	data: {
+		user: null,
+	}
+});
+
 
 function onDeviceReady()
 {
 
 }
 
-function fbLoginSuccess()
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+		console.log(response);
+	});
+}
+
+var fbLoginSuccess = function (userData)
 {
+	//console.log(userData);
+	FB.api('/me?fields=id,cover,name,first_name,last_name,gender,age_range,link,locale,picture,verified,email,birthday,likes', function(response) {
+		console.log(response);
+	});
 	$('#status').html('Login success');
 }
 
 function loginToFacebook()
 {
-	facebookConnectPlugin.login(["public_profile"],
+	facebookConnectPlugin.login(["public_profile", "user_birthday", "user_likes", "email"],
 		fbLoginSuccess,
 		function (err) {
 			$('#status').html('Authentication fail');
@@ -33,6 +64,14 @@ function logout()
 	});
 }
 
+function loginStatus()
+{
+	facebookConnectPlugin.getLoginStatus(fbLoginSuccess, function (err) {
+		console.log(err);
+	});
+}
+
 $( document ).ready(function(){
+
 	$(".button-collapse").sideNav();
 })
